2723a86731a6c86943b9a2481b0da432f7b0e206
Dineth0 <dinethnakandala10@gmail.com>
2025-08-24 20:41:56 +0530

    add workshopRegistration entity

---
+++ Backend/src/main/java/lk/ijse/gdse/traditionalexperiencebackend/entity/WorkshopRegistration.java
@@ -0,0 +1,33 @@
+package lk.ijse.gdse.traditionalexperiencebackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.sql.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class WorkshopRegistration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String phone;
+    private String country;
+    private Date registrationDate;
+    private Date selectWorkshopDate;
+    private int member;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "workshopId")
+    private Workshop workshop;
+}
