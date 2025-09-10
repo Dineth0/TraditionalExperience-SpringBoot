ef1ab1197e0d931ed6b58df996515503deffdfba
Dineth0 <dinethnakandala10@gmail.com>
2025-09-10 14:30:13 +0530

    add email entity

---
+++ Backend/src/main/java/lk/ijse/gdse/traditionalexperiencebackend/entity/Email.java
@@ -0,0 +1,26 @@
+package lk.ijse.gdse.traditionalexperiencebackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Email {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String subject;
+    private String body;
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "instructorId")
+    private Instructor instructor;
+
+}
