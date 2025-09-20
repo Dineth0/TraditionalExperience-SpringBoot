1bbbaa4d4c2a806a6fd39f0e4681700970a0d706
Dineth0 <dinethnakandala10@gmail.com>
2025-09-20 11:09:22 +0530

    add exception

---
+++ Backend/src/main/java/lk/ijse/gdse/traditionalexperiencebackend/advisor/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package lk.ijse.gdse.traditionalexperiencebackend.advisor;
+
+import lk.ijse.gdse.traditionalexperiencebackend.util.ResponseUtil;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ResponseUtil> handleGenericRunTimeException(Exception e) {
+        ResponseUtil responseUtil = new ResponseUtil();
+        responseUtil.setCode(500);
+        responseUtil.setMsg(e.getMessage());
+        return new ResponseEntity<>(responseUtil, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IdNotFoundException.class)
+    public ResponseEntity<ResponseUtil> handleIdNotFoundException(MethodArgumentNotValidException e) {
+        ResponseUtil responseUtil = new ResponseUtil();
+        responseUtil.setCode(500);
+        responseUtil.setMsg(e.getMessage());
+        return new ResponseEntity<>(responseUtil, HttpStatus.NOT_FOUND);
+    }
+
+}
---
+++ Backend/src/main/java/lk/ijse/gdse/traditionalexperiencebackend/advisor/IdNotFoundException.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse.traditionalexperiencebackend.advisor;
+
+public class IdNotFoundException extends Throwable {
+}
